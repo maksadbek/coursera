09155fa64c7c2c9d5a58e7f8ec32b152e7d53094
Максадбек Ахмедов <makhmedov@ivi.ru>
2018-12-24 18:55:44 +0300

    Solution for shortest paths problem
    Using Bellman-Ford and then BFS to traverse the negative cycle.

--- graphs/week4/paths_in_graphs_starter_files_2/shortest_paths/shortest_paths.cpp
+++ graphs/week4/paths_in_graphs_starter_files_2/shortest_paths/shortest_paths.cpp
@@ -2,40 +2,119 @@
 #include <limits>
 #include <vector>
 #include <queue>
+#include <utility>
+#include <cstring>
 
 using std::vector;
 using std::queue;
 using std::pair;
 using std::priority_queue;
+using std::make_pair;
+using std::string;
+using std::to_string;
+using std::numeric_limits;
 
-vector<string> shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, ) {
-  //write your code here
+const int max_int = numeric_limits<int>::max();
+
+vector<string> shortest_paths(vector<vector<pair<int,int>>> &adj, int s) {
+	vector<long long> dist(adj.size(), max_int);
+	dist[s] = 0;
+
+	for(int i = 0; i < adj.size()-1; i++) {
+		for(int u = 0; u < adj.size(); u++) {
+			for(auto v : adj[u]) {
+				int to = v.first;
+				int w = v.second;
+
+				if(dist[u] != max_int && dist[u] + w < dist[to]) {
+					dist[to] = dist[u] + w;
+				}
+			}
+		}
+	}
+
+	vector<string> result(adj.size());
+	result[s] = "0";
+
+	queue<int> q;
+        for(int i = 0; i < 1; i++) {
+		for(int u = 0; u < adj.size(); u++) {
+			for(auto v : adj[u]) {
+				int to = v.first;
+				int w = v.second;
+
+				if(dist[u] != max_int && dist[u] + w < dist[to]) {
+					dist[to] = dist[u] + w;
+					q.push(to);
+				}
+			}
+		}
+        }
+
+	vector<int> neg;
+	vector<bool> used(adj.size(), false);
+	
+	// do a bfs and find vertexes of negative cycles
+	while(!q.empty()) {
+	  int u = q.front();
+	  q.pop();
+	  
+	  if(used[u]) {
+	    continue;
+	  }
+
+	  used[u] = true;
+	  neg.push_back(u);
+	  
+	  for(auto v : adj[u]) {
+	    if(!used[v.first]) {
+	      q.push(v.first);
+	    }
+	  }
+	}
+
+        for(int i = 0; i < dist.size(); i++) {
+                if(dist[i] == max_int) {
+                        result[i] = "*";
+			continue;
+                }
+
+		bool negative = false;
+		for(int x : neg) {
+		  if(x == i) {
+		    negative = true;
+		    break;
+		  }
+		}
+
+		if(negative) {
+		  result[i] = "-";
+		  continue;
+		}
+
+		result[i] = to_string(dist[i]);
+        }
+
+	return result;
 }
 
 int main() {
-  int n, m, s;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y, w;
-    std::cin >> x >> y >> w;
-    adj[x - 1].push_back(y - 1);
-    cost[x - 1].push_back(w);
-  }
-  std::cin >> s;
-  s--;
-  vector<long long> distance(n, std::numeric_limits<long long>::max());
-  vector<int> reachable(n, 0);
-  vector<int> shortest(n, 1);
-  shortest_paths(adj, cost, s, distance, reachable, shortest);
-  for (int i = 0; i < n; i++) {
-    if (!reachable[i]) {
-      std::cout << "*\n";
-    } else if (!shortest[i]) {
-      std::cout << "-\n";
-    } else {
-      std::cout << distance[i] << "\n";
-    }
-  }
+	int n, m, s;
+	std::cin >> n >> m;
+
+	vector<vector<pair<int,int>>> adj(n, vector<pair<int,int>>());
+
+	for (int i = 0; i < m; i++) {
+		int x, y, w;
+		std::cin >> x >> y >> w;
+
+		adj[x - 1].push_back(make_pair(y - 1, w));
+	}
+
+	std::cin >> s;
+
+	auto result = shortest_paths(adj, s-1);
+	for (auto x : result ) {
+		std::cout << x << "\n";
+	}
 }
