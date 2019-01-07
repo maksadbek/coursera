b5f446c78dbef526193b34c4442f52b4563ad95d
Максадбек Ахмедов <makhmedov@ivi.ru>
2019-01-07 21:08:25 +0300

    fixed bidijkstra

--- graphs/week6/bidijkstra.cpp
+++ graphs/week6/bidijkstra.cpp
@@ -1,120 +1,179 @@
 #include <iostream>
 #include <vector>
-#include <utility>
-#include <limits>
-#include <algorithm>
-#include <iterator>
 #include <set>
-#include <queue>
+#include <limits>
 #include <utility>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-typedef pair<long long, int> int_pair;
+typedef long long ll;
+typedef pair<ll, int> llp;
+typedef vector<vector<llp>> graph;
+typedef priority_queue<llp, vector<llp>, greater<llp>> pqueue;
 
-const long long inf = numeric_limits<long long>::max() / 4;
-const bool debug = true;
+const ll inf = numeric_limits<ll>::max()/4;
 
-vector<vector<int_pair>> adj;
-vector<vector<int_pair>> adj_r;
+struct Bidijkstra {
+  graph adjs[2];
+  pqueue q[2];
+  vector<ll> dist[2];
 
-set<int> proc;
+  vector<int> prev[2];
+  set<int> proc;
+  vector<bool> visited[2];
 
-long long shortest_path(int s, int t, vector<long long> &dist, vector<long long> &dist_r) {
-	auto distance = inf;
-	for(auto v : proc) {
-		auto d = dist[v] + dist_r[v];
-		if(d > inf || d < 0) {
-			continue;
-		}
+  Bidijkstra(graph &adj, graph r_adj) {
+    adjs[0] = adj;
+    adjs[1] = r_adj;
 
-		if(d <= distance) {
-			distance = d;
-		}
-	}
+    dist[0].resize(adj.size(), inf);
+    dist[1].resize(adj.size(), inf);
 
-	if(distance == inf) {
-		return -1;
-	}
-	
-	return distance;
+    prev[0].resize(adj.size());
+    prev[1].resize(adj.size());
+
+    visited[0].resize(adj.size(), false);
+    visited[1].resize(adj.size(), false);
+  }
+
+  ll shortest_path(int s, int t) {
+    ll d = inf;
+    int best = -1;
+
+    cout <<" insert inot proc: " << proc.insert(98).second << endl;
+    
+    for(auto u : proc) {
+      printf("%d, to %lld, from %lld *** ", u, dist[0][u], dist[1][u]);
+      printf("d was %lld ", d);
+
+      if(dist[0][u] + dist[1][u] < d) {
+	best = u;
+	d = dist[0][u] + dist[1][u];
+      }
+
+      printf("become: %lld", d);
+      printf("\n");
+    }
+    
+    if(d == inf) {
+      return -1;
+    }
+
+    vector<int> path;
+    int last = best;
+    while(last != s) {
+      path.push_back(last);
+      last = prev[0][last];
+    }
+    reverse(path.begin(), path.end());
+    
+    last = best;
+
+    while(last != t) {
+      last = prev[1][last];
+      path.push_back(last);
+    }
+
+    printf("path: ");
+    for(auto x : path) {
+      printf("%d -> ", x);
+    }
+    printf("\n");
+
+    printf(">>>>>>>> to 98 %lld, from %lld <<<<<<\n", dist[0][98], dist[1][98]);
+    
+					    
+    return d;
+  }
+
+  ll visit(int side, int s, int t) {
+    auto u = q[side].top();
+    q[side].pop();
+
+    if(visited[side][u.second]) {
+      return -2;
+    }
+
+    visited[side][u.second] = true;
+    printf("%d %d w/ w of %lld\n", side, u.second, u.first);
+    for(auto next : adjs[side][u.second]) {
+      auto w = next.first;
+      auto v = next.second;
+      printf("side: %d; adj of %d -> %d; \n", side, u.second, v);
+      if(u.first + w < dist[side][v]) {
+	dist[side][v] = dist[side][u.second] + w;
+	printf("dist to %lld\n", dist[side][v]);
+	q[side].push(make_pair(dist[side][v], v));
+	prev[side][v] = u.second;
+      }
+    }
+
+    if(!proc.insert(u.second).second) {
+      printf("meet-in-the-middle: %d\n", u.second);
+      return shortest_path(s, t);
+    }
+
+    return -2;
+  }
+
+  void clear() {
+    q[0] = pqueue();
+    q[1] = pqueue();
+    proc.clear();
+    prev[0].clear();
+    prev[1].clear();
+
+    dist[0].assign(adjs[0].size(), inf);
+    dist[1].assign(adjs[0].size(), inf);
+  }
+  
+  ll query(int s, int t) {
+    clear();
+    
+    dist[0][s] = 0;
+    dist[1][t] = 0;
+
+    q[0].push(make_pair(0, s));
+    q[1].push(make_pair(0, t));
+
+    while(true) {
+      auto d = visit(0, s, t);
+      if(d != -2) {
+	return d;
+      }
+
+      auto d_r = visit(1, s, t);
+      if(d_r != -2) {
+	return d_r;
+      }
+    }
+  }
 };
 
-long long bidijkstra(int s, int t) {
-	proc.clear();
-	
-	vector<long long> dist(adj.size(), inf);
-	vector<long long> dist_r(adj.size(), inf);
-	dist[s] = 0;
-	dist_r[t] = 0;
-	
-	priority_queue<int_pair, vector<int_pair>, greater<int_pair>> q;
-	q.push(make_pair(0, s));
-
-	priority_queue<int_pair, vector<int_pair>, greater<int_pair>> q_r;
-	q_r.push(make_pair(0, t));
-	
-	while(true) {
-		auto v = q.top();
-		q.pop();
-
-		if(v.first==inf) {
-		  continue;
-		}
-		
-		for(auto e : adj[v.second]) {
-			auto d = e.first + v.first;
-			if(d < dist[e.second]) {
-				q.push(make_pair(d, e.second));
-				dist[e.second] = d;
-			}
-		}
-		
-		if(!proc.insert(v.second).second) {
-			return shortest_path(s, t, dist, dist_r);
-		}
-
-		auto v_r  = q_r.top();
-		q_r.pop();
-
-		if(v_r.first == inf) {
-		  continue;
-		}
-		for(auto e : adj_r[v_r.second]) {
-			auto d = e.first + v_r.first;
-			if(d < dist_r[e.second]) {
-				dist_r[e.second] = d;
-				q_r.push(make_pair(d, e.second));
-			}
-		}
-		
-		if(!proc.insert(v_r.second).second) {
-			return shortest_path(s, t, dist, dist_r);
-		}
-	}
-
-	return -1;
-}
-
 int main() {
-	int n, m;
-	scanf("%d%d", &n, &m);
-	adj.resize(n);
-	adj_r.resize(n);
+  int n, m;
+  scanf("%d%d", &n, &m);
 	
-	for (int i=0; i<m; ++i) {
-		int u, v, c;
-		scanf("%d%d%d", &u, &v, &c);
+  graph adj(n);
+  graph r_adj(n);
+	
+  for (int i=0; i<m; ++i) {
+    int u, v, c;
+    scanf("%d%d%d", &u, &v, &c);
 		
-		adj[u-1].push_back(make_pair(c, v-1));
-		adj_r[v-1].push_back(make_pair(c, u-1));
-	}
+    adj[u-1].push_back(make_pair(c, v-1));
+    r_adj[v-1].push_back(make_pair(c, u-1));
+  }
 
-	int t;
-	scanf("%d", &t);
-	for (int i=0; i< t; ++i) {
-		int u, v;
-		scanf("%d%d", &u, &v);
-		printf("%lld\n", bidijkstra(u-1, v-1));
-	}
+  Bidijkstra bidijkstra(adj, r_adj);
+  
+  int t;
+  scanf("%d", &t);
+  for (int i=0; i< t; ++i) {
+    int u, v;
+    scanf("%d%d", &u, &v);
+    printf("%lld\n", bidijkstra.query(u-1, v-1));
+  }
 }
